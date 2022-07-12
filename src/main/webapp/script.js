a62a600f6368e576d979fc3fadfae59d18a0296c
PJ Sethbhakdi <pjseth@github.com>
2022-07-12 02:18:39 +0000

    implement jsPDF function

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -49,20 +49,39 @@ function jumpToPage1() {
 function jumpToPage2() {
     window.location.href="page_2.html";
 }
-//DOWNLOAD PAGE
-var doc = new jsPDF(); 
-var specialElementHandlers = {
-    '#editor': function (element, renderer) {
-        return true;
-    }
-};
-//margins.left, // x coord   margins.top, { // y coord
+
 function download() {
-    doc.fromHTML($('#mid').html(), 15, 15, {
-        'width': 700,
-        'elementHandlers': specialElementHandlers
-    });
-    doc.save('ASL_result.pdf');
+    var pdf = new jsPDF('p', 'pt', 'letter');
+    source = document.getElementById('content2').innerHTML;
+    specialElementHandlers = {
+        // element with id of "bypass" - jQuery style selector
+        '#editor': function (element, renderer) {
+            // true = "handled elsewhere, bypass text extraction"
+            return true
+        }
+    };
+    margins = {
+        top: 80,
+        bottom: 60,
+        left: 40,
+        width: 522
+    };
+    // all coords and widths are in jsPDF instance's declared units
+    // 'inches' in this case
+    pdf.fromHTML(
+        source, // HTML string or DOM elem ref.
+        margins.left, // x coord
+        margins.top, { // y coord
+            'width': margins.width, // max width of content on PDF
+            'elementHandlers': specialElementHandlers
+        },
+
+        function (dispose) {
+            // dispose: object with X, Y of the last line add to the PDF 
+            //          this allow the insertion of new lines after html
+            pdf.save('ASL.pdf');
+        }, margins
+    );        
 };
 
 
